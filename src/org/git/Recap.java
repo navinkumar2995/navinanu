c7ff1665321ac06310e039d866486514dd8ed24c
navinkumar2995 <snk.king95@gmail.com>
2023-02-27 11:30:25 +0530

    add new line

--- src/org/git/Recap.java
+++ src/org/git/Recap.java
@@ -1,5 +1,12 @@
 package org.git;
 
 public class Recap {
+	public static void main(String[] args) {
+		System.out.println("first");
+	}
+	private void second() {
+		System.out.println("second");
+
+	}
 
 }
